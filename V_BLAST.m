84e2cd4fba3824388d8de3236dd05bc49845c11b
TRAPARIC David <david.traparic@enseirb-matmeca.fr>
2020-11-19 12:15:52 +0100

    g le cervo qui fume

--- V_BLAST.m
+++ V_BLAST.m
@@ -12,17 +12,17 @@ K = L*nb_mdc*M_psk;
 phi0 = 0;
 varv = 1; % variance bruit
 
-% EbN0dB_min  = -2; % Minimum de EbN0
-% EbN0dB_max  = 10; % Maximum de EbN0
-% EbN0dB_step = 1;% Pas de EbN0
-%
-% nbr_erreur  = 100;  % Nombre d'erreurs a observer avant de calculer un BER
-% nbr_bit_max = 100e6;% Nombre de bits max a simuler
-% ber_min     = 1e-6; % BER min
-%
-% EbN0dB = EbN0dB_min:EbN0dB_step:EbN0dB_max;     % Points de EbN0 en dB a simuler
-% EbN0   = 10.^(EbN0dB/10);% Points de EbN0 a simuler
-% sigma2 = 1/(M_psk*EbN0);
+EbN0dB_min  = -2; % Minimum de EbN0
+EbN0dB_max  = 10; % Maximum de EbN0
+EbN0dB_step = 1;% Pas de EbN0
+
+nbr_erreur_cap = 100;  % Nombre d'erreurs a observer avant de calculer un BER
+nbr_bit_max = 100e6;% Nombre de bits max a simuler
+ber_min     = 1e-6; % BER min
+
+EbN0dB = EbN0dB_min:EbN0dB_step:EbN0dB_max;     % Points de EbN0 en dB a simuler
+EbN0   = 10.^(EbN0dB/10);% Points de EbN0 a simuler
+sigma2 = 1./EbN0;
 
 %% Construction du modulateur
 mod_psk = comm.PSKModulator(...
@@ -64,47 +64,64 @@ end
 % H = sqrt(varv/2)*(randn(M,N) + 1i*randn(M,N)); % h une 2X2 : les coefs des différents "sous-canaux" créés avec un coef par sous-canal
 % on les regénère a chaque fois donc on va le foutre dans la boucle
 
-b = randi([0,1],K,1);    % Generation du message al�atoire
+nb_errs = zeros(size(EbN0));
 
-x_stream = mod_psk(b);
+ber = zeros(size(EbN0));
+nb_err = 0;
+for i_snr = 1:length(EbN0)
+    
+    i_snr
+    varv = sigma2(i_snr)
+    nb_bits_sent = 0;
+    nb_err = 0;
+    cmp = 0;
+    while (nb_err < nbr_erreur_cap)
+        cmp = cmp +1;
+        %% chaine de com
+        for i=1:nb_mdc
 
-%% attention, voilà le gros encodage spatiotemporel de la mort qui tue V_BLAST
-meta_X = reshape(x_stream, N, []); % la première 2x2 = le premier mdc=X, il y a nb_mdc matrices 2x2
-%% Voila
 
-% meta_Y = []
+            %% emission
+            b = randi([0,1],K,1);    % Generation du message al�atoire
 
+            x_stream = mod_psk(b);
 
-%% chaine de com
-for i=1:nb_mdc
+            %% attention, voilà le gros encodage spatiotemporel de la mort qui tue V_BLAST
+            meta_X = reshape(x_stream, N, []); % la première 2x2 = le premier mdc=X, il y a nb_mdc matrices 2x2
+            % Voila
 
+            X = meta_X(1:2, i:i+L-1);
 
-    %% emission
-    X = meta_X(1:2, i:i+L-1);
+            %% canal
+            H = sqrt(varv/2)*(randn(M,N) + 1i*randn(M,N));
+            V = sqrt(varv/2)*(randn(M,L) + 1i*randn(M,L)); % autant de bruit thermiques de capteurs recepteurs
 
-    %% canal
-    H = sqrt(varv/2)*(randn(M,N) + 1i*randn(M,N));
-    V = sqrt(varv/2)*(randn(M,L) + 1i*randn(M,L)); % autant de bruit thermiques de capteurs recepteurs
+            Y = H*X + V;
 
-    Y = H*X + V;
+            %% Reception
+            i_min = 1;
+            for i_codepossible = 1:4^4 % optimisable
 
-    %% Reception
-    i_min = 1;
-    for i_codepossible = 1:4^4 % optimisable
+                X_tmp = C(:,:,i_codepossible);
+                mynorm = norm(Y - H*X_tmp, 'fro');
 
-        X_tmp = C(:,:,i_codepossible);
-        mynorm = norm(Y - H*X_tmp, 'fro');
+                if i_codepossible == 1
+                    min = mynorm;
+                elseif min > mynorm
+                    min = mynorm;
+                    i_min = i_codepossible;
+                end
 
-        if i_codepossible == 1
-            min = mynorm;
-        elseif min > mynorm
-            min = mynorm;
-            i_min = i_codepossible;
+            end
+            X_chap = C(:,:,i_min);
+
+            nb_err = nb_err + any((X_chap ~= X), 'all'); % c'est pas un taux d'erreur binaire c'est un taux d'erreur mdc
         end
-
+        nb_bits_sent = cmp*nb_mdc;
+        nb_errs(i_snr) = nb_err;
+        ber(i_snr)= nb_errs(i_snr)/nb_bits_sent;
     end
-    X_chap = C(:,:,i_min);
-
-
 
 end
+
+semilogy(EbN0dB, ber);
