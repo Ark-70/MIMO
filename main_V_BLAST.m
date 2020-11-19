70e5bbdb72b4587a7efa588b6fe0f84bfaa4597d
TRAPARIC David <david.traparic@enseirb-matmeca.fr>
2020-11-19 11:17:12 +0100

    on est pas mal bambam

---
+++ main_V_BLAST.m
@@ -0,0 +1,167 @@
+clear
+clc
+
+%% Parametres
+% -------------------------------------------------------------------------
+addpath('src')
+simulation_name = 'non_codee';
+
+R = 1; % Rendement de la communication
+
+pqt_par_trame = 1; % Nombre de paquets par trame
+bit_par_pqt   = 330; % Nombre de bits par paquet
+L = 2 %
+K = pqt_par_trame*bit_par_pqt; % Nombre de bits de message par trame
+N = K/R; % Nombre de bits cod�s par trame (cod�e)
+
+M_psk = 4; % Modulation BPSK <=> 2 symboles
+phi0 = 0; % Offset de phase our la BPSK
+
+EbN0dB_min  = -2; % Minimum de EbN0
+EbN0dB_max  = 10; % Maximum de EbN0
+EbN0dB_step = 1;% Pas de EbN0
+
+nbr_erreur  = 100;  % Nombre d'erreurs � observer avant de calculer un BER
+nbr_bit_max = 100e6;% Nombre de bits max � simuler
+ber_min     = 1e-6; % BER min
+
+EbN0dB = EbN0dB_min:EbN0dB_step:EbN0dB_max;     % Points de EbN0 en dB � simuler
+EbN0   = 10.^(EbN0dB/10);% Points de EbN0 � simuler
+% sigma2 = 1/(M_psk*EbN0);
+% -------------------------------------------------------------------------
+
+%% Construction du modulateur
+mod_psk = comm.PSKModulator(...
+    'ModulationOrder', M, ... % BPSK
+    'PhaseOffset'    , phi0, ...
+    'SymbolMapping'  , 'Gray',...
+    'BitInput'       , true);
+
+%% Construction du demodulateur
+demod_psk = comm.PSKDemodulator(...
+    'ModulationOrder', M, ...
+    'PhaseOffset'    , phi0, ...
+    'SymbolMapping'  , 'Gray',...
+    'BitOutput'      , true,...
+    'DecisionMethod' , 'Log-likelihood ratio');
+
+%% Construction du canal AWGN
+awgn_channel = comm.AWGNChannel(...
+    'NoiseMethod', 'Signal to noise ratio (Es/No)',...
+    'EsNo',EsN0dB(1),...
+    'SignalPower',1);
+
+%% Construction de l'objet �valuant le TEB
+stat_erreur = comm.ErrorRate(); % Calcul du nombre d'erreur et du BER
+
+%% Initialisation des vecteurs de r�sultats
+ber = zeros(1,length(EbN0dB));
+Pe = qfunc(sqrt(2*EbN0));
+
+%% Pr�paration de l'affichage
+figure(1)
+h_ber = semilogy(EbN0dB,ber,'XDataSource','EbN0dB', 'YDataSource','ber');
+hold all
+ylim([1e-6 1])
+grid on
+xlabel('$\frac{E_b}{N_0}$ en dB','Interpreter', 'latex', 'FontSize',14)
+ylabel('TEB','Interpreter', 'latex', 'FontSize',14)
+
+%% Pr�paration de l'affichage en console
+msg_format = '|   %7.2f  |   %9d   |  %9d | %2.2e |  %8.2f kO/s |   %8.2f kO/s |   %8.2f s |\n';
+
+fprintf(      '|------------|---------------|------------|----------|----------------|-----------------|--------------|\n')
+msg_header =  '|  Eb/N0 dB  |    Bit nbr    |  Bit err   |   TEB    |    Debit Tx    |     Debit Rx    | Tps restant  |\n';
+fprintf(msg_header);
+fprintf(      '|------------|---------------|------------|----------|----------------|-----------------|--------------|\n')
+
+
+%% Simulation
+for i_snr = 1:length(EbN0dB)
+    reverseStr = ''; % Pour affichage en console
+    awgn_channel.EsNo = EsN0dB(i_snr);% Mise a jour du EbN0 pour le canal
+
+    stat_erreur.reset; % reset du compteur d'erreur
+    err_stat    = [0 0 0]; % vecteur r�sultat de stat_erreur
+
+    demod_psk.Variance = awgn_channel.Variance;
+
+    n_frame = 0;
+    T_rx = 0;
+    T_tx = 0;
+    general_tic = tic;
+    while (err_stat(2) < nbr_erreur && err_stat(3) < nbr_bit_max)
+        n_frame = n_frame + 1;
+
+        %% Emetteur
+        tx_tic = tic;                 % Mesure du d�bit d'encodage
+        b    = randi([0,1],K,1);    % G�n�ration du message al�atoire
+        x      = step(mod_psk,  b); % Modulation BPSK
+        T_tx   = T_tx+toc(tx_tic);    % Mesure du d�bit d'encodage
+
+        %% Canal
+        y     = step(awgn_channel,x); % Ajout d'un bruit gaussien
+
+        %% Recepteur
+        rx_tic = tic;                  % Mesure du d�bit de d�codage
+        Lc      = step(demod_psk,y);   % D�modulation (retourne des LLRs)
+
+%         H = alist2sparse('alist/DEBUG_6_3.alist');
+        propagate_decod(H, Lc);
+
+        rec_b = double(Lc(1:K) < 0); % D�cision
+        T_rx    = T_rx + toc(rx_tic);  % Mesure du d�bit de d�codage
+
+        err_stat   = step(stat_erreur, b, rec_b); % Comptage des erreurs binaires
+
+        %% Affichage du r�sultat
+        if mod(n_frame,100) == 1
+            msg = sprintf(msg_format,...
+                EbN0dB(i_snr),         ... % EbN0 en dB
+                err_stat(3),           ... % Nombre de bits envoy�s
+                err_stat(2),           ... % Nombre d'erreurs observ�es
+                err_stat(1),           ... % BER
+                err_stat(3)/8/T_tx/1e3,... % D�bit d'encodage
+                err_stat(3)/8/T_rx/1e3,... % D�bit de d�codage
+                toc(general_tic)*(nbr_erreur - min(err_stat(2),nbr_erreur))/(min(err_stat(2),nbr_erreur))); % Temps restant
+            fprintf(reverseStr);
+            msg_sz =  fprintf(msg);
+            reverseStr = repmat(sprintf('\b'), 1, msg_sz);
+        end
+
+    end
+
+    msg = sprintf(msg_format,...
+        EbN0dB(i_snr),         ... % EbN0 en dB
+        err_stat(3),           ... % Nombre de bits envoy�s
+        err_stat(2),           ... % Nombre d'erreurs observ�es
+        err_stat(1),           ... % BER
+        err_stat(3)/8/T_tx/1e3,... % D�bit d'encodage
+        err_stat(3)/8/T_rx/1e3,... % D�bit de d�codage
+        0); % Temps restant
+    fprintf(reverseStr);
+    msg_sz =  fprintf(msg);
+    reverseStr = repmat(sprintf('\b'), 1, msg_sz);
+
+    ber(i_snr) = err_stat(1);
+    refreshdata(h_ber);
+    drawnow limitrate
+
+    if err_stat(1) < ber_min
+        break
+    end
+
+end
+fprintf('|------------|---------------|------------|----------|----------------|-----------------|--------------|\n')
+
+%%
+figure(1)
+semilogy(EbN0dB,ber);
+hold all
+xlim([0 10])
+ylim([1e-6 1])
+grid on
+xlabel('$\frac{E_b}{N_0}$ en dB','Interpreter', 'latex', 'FontSize',14)
+ylabel('TEB','Interpreter', 'latex', 'FontSize',14)
+
+save(simulation_name,'EbN0dB','ber')
